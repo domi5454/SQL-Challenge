af11542ea331111c03e2d97e82b507503eea7234
Ding5454 <domingorodriguez2012@gmail.com>
2019-10-15 16:06:13 -0500

    Got all query scripts and csv's done.  Still need SQLAlchemy bonus

--- retrieveDeptManagers.sql
+++ retrieveDeptManagers.sql
@@ -4,10 +4,20 @@ departments.dept_name as Department,
 dept_manager.emp_no as EmployeeNO,
 employees.last_name as LastName,
 employees.first_name as FirstName,
+titles.title as Title,
 dept_manager.from_date as StartDate,
 dept_manager.to_date as EndDate
 
-FROM dept_manager, departments, employees
-WHERE dept_manager.emp_no = employees.emp_no AND
-dept_manager.dept_no = departments.dept_no
+FROM 
+dept_manager, 
+departments, 
+employees,
+titles
+
+WHERE 
+dept_manager.emp_no = employees.emp_no AND
+dept_manager.emp_no = titles.emp_no AND
+dept_manager.dept_no = departments.dept_no AND
+titles.title NOT LIKE 'Staff'
+
 ORDER BY dept_manager.emp_no ASC;
